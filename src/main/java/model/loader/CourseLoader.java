4945a17ac1907297c8c18b3e2b2b1d98d40e3c17
Nikita Dizhur <blenderon@gmail.com>
2016-12-07 02:59:24 +0200

    made of CourseLoader an abstract class #2

--- src/main/java/model/loader/CourseLoader.java
+++ src/main/java/model/loader/CourseLoader.java
@@ -1,15 +1,28 @@
 package model.loader;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 import model.course.Course;
 
 /**
- * Interface for loading courses data from outside.
- * It can be implemented as e.g.: XmlCourseLoader, UrlCourseLoader, etc...
+ * Abstract class for loading courses data from outside.
+ * It can be implemented as e.g.: XmlCourseLoader, UrlCourseLoader, JsonLoader etc...
  * */
-public interface CourseLoader {
+public abstract class CourseLoader {
 	//TODO: I'm not sure, whether it should be a List or HashMap
 	// apparently it's a hashMap with mapping (courseName -> course)
-	HashMap<String, Course> loadCourseFrom(String path);
+	protected HashMap<String, Course> courses;
+	
+	public abstract HashMap<String, Course> loadCoursesFrom(String path);
+	
+	// TODO: make this method final?
+	public Optional<Course> getLoadedCourse(String name) {
+		return Optional.of(courses.get(name));
+	}
+	
+	// TODO: make this method final?
+	public HashMap<String, Course> getAllLoadedCourses() {
+		return new HashMap<>(courses);
+	}
 }
