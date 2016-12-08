bd1681b7a67e772c0df4ce273c22f5415d614fa6
Nikita Dizhur <blenderon@gmail.com>
2016-12-08 19:15:23 +0200

    add description to Controller #19

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,6 +7,12 @@ import view.View;
 /**
  * It's the main class that speaks with View-part of the program.
  * It interacts with UI and updates Models.
+ * 
+ * The GUI parts which do not update when the model changes - are the responsibility of the controller.
+ * When you try to modify a field in the view, the controller needs to pick up the editing event,
+ * process it, and send it to the model; the model will then update the view if/when the value actually changes.
+ * In many frameworks, the controller appears as a collection of methods and listeners built into both
+ * the data model and the view.
  * */
 public class Controller {
 	Model model;
