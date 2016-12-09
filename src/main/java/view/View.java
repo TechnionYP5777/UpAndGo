b5a78f3ac7cca4bc1e39446f2b29519cdac07b68
Nikita Dizhur <blenderon@gmail.com>
2016-12-09 04:39:10 +0200

    defined method addListener(ActionListener l) in View interface #2

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,7 +1,12 @@
 package view;
 
+import java.awt.event.ActionListener;
+
 public interface View {
 	// TODO: implement
 	// I think GUI consists of these Views.
 	// And maybe it will be good to use Composition Pattern for them.
+	
+	void addListener(ActionListener l);
+	
 }
