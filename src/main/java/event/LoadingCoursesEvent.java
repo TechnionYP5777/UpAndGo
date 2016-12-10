558cb85e4e30fb05e49a3026f476bc74d82a9912
Nikita Dizhur <blenderon@gmail.com>
2016-12-10 22:14:17 +0200

    extended EventObject class in package event.
    still need to implement them #25

---
+++ src/main/java/event/LoadingCoursesEvent.java
@@ -0,0 +1,19 @@
+package event;
+
+import java.util.EventObject;
+
+public class LoadingCoursesEvent extends EventObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6403319306242837478L;
+
+	public LoadingCoursesEvent(Object source) {
+		super(source);
+		// TODO Auto-generated constructor stub
+	}
+	
+//	TODO: public List<Course> getCourses() {...}
+
+}
---
+++ src/main/java/event/SavingChosenCoursesEvent.java
@@ -0,0 +1,17 @@
+package event;
+
+import java.util.EventObject;
+
+public class SavingChosenCoursesEvent extends EventObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8810499185002995020L;
+
+	public SavingChosenCoursesEvent(Object source) {
+		super(source);
+		// TODO Auto-generated constructor stub
+	}
+//	TODO: public List<Course> getCourses() {...}
+}
