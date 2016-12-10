f5428dcdceb8cb19ed4739c81a432e00f9674418
OmriBS <omri.benshmuel@gmail.com>
2016-12-10 22:12:24 +0200

    implementation of Model Interface #18

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,16 +1,67 @@
 package model;
 
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import model.course.Course;
+import model.loader.CourseLoader;
 
 /**
  * Interface for storing data inside the program.
  * The data typically should come from loader.
  * */
-public interface Model {
+public abstract class Model {
 	// TODO: implement
 	// Maybe it will be good to use Observer pattern to notify views when the Model changes.
 	// smth like:
 	// addObservers(), notifyObservers()
 	
-	void addListener(ActionListener l);
+	protected List<Course> courseList;
+	protected List<ActionListener> listenersList;
+	
+	public Model(){
+		this.courseList = new ArrayList<>();
+		this.listenersList = new ArrayList<>();
+	}
+	
+	void addListener(ActionListener ¢) {
+		if (¢ == null)
+			throw new NullPointerException();
+		this.listenersList.add(¢);
+	}
+	
+	public void pickCourse(String name) {
+		if (name == null)
+			throw new NullPointerException();
+		this.courseList.add(CourseLoader.getLoadedCourse(name));
+	}
+	
+	public List<String> getCoursesNames() {
+		List<String> $ = new ArrayList<>();
+		for(Course it:courseList)
+			$.add(it.getName());
+		return $;
+	}
+	
+	public void dropCourse(String name) {
+		if (name == null)
+			throw new NullPointerException();
+		for(Course it:courseList)
+			if (it.getName().equals(name)) {
+				this.courseList.remove(it);
+				return;
+			}
+		return;
+	}
+	
+	public Course getCourseByName(String name) {
+		if (name == null)
+			throw new NullPointerException();
+		for(Course $:courseList)
+			if ($.getName().equals(name))
+				return $;
+		return null;
+	}
 }
