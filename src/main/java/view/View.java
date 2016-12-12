6c2f93bf5d9a538ddc167ed911a340427940445a
Nikita Dizhur <blenderon@gmail.com>
2016-12-12 02:17:01 +0200

    add method addObserver to View #1

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -8,7 +9,15 @@ public abstract class View extends Observable implements Observer {
 	// I think GUI consists of these Views.
 	// And maybe it will be good to use Composition Pattern for them.
 	
+	protected List<Observer> listenersList;
+	
 	@Override
 	public abstract void update(Observable obj, Object arg);
 	
+	@Override
+	public synchronized void addObserver(Observer ¢) {
+		if (¢ == null)
+			throw new NullPointerException();
+		this.listenersList.add(¢);
+	}
 }
