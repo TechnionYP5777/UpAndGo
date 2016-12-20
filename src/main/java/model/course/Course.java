d0657a62c528eecec04294a546afddea94b3343e
SapirBismot <Sapirbismot@gmail.com>
2016-12-20 13:02:13 +0200

    add the prerequisites list of a course #53

--- src/main/java/model/course/Course.java
+++ src/main/java/model/course/Course.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 // after object creation one can only add or remove lessons
 // TODO: do we need to change it?
 public class Course {
-	
+
 	protected final String name;
 	protected final String id;
 	protected final String faculty;
@@ -20,56 +20,60 @@ public class Course {
 
 	protected final List<StuffMember> stuff;
 	protected final List<Lesson> lessons;
-	
+
 	protected int lectureHours;
 	protected int tutorialHours;
 	protected int laboratoryHours;
 	protected int projectHours;
-	
+
 	protected final List<CourseListener> listeners;
-	
-	//TODO: create interface LessonGroup as mediator between Lessons and Course
-	
-	public Course(String name1, String id1, String faculty1, List<StuffMember> st, int acPoints,
-																		LocalDateTime aT, LocalDateTime bT) {
-		
-		if((name1==null) || (faculty1==null))
+
+	protected final List<Course> prerequisites;
+
+	// TODO: create interface LessonGroup as mediator between Lessons and Course
+
+	public Course(String name1, String id1, String faculty1, List<StuffMember> st, int acPoints, LocalDateTime aT,
+			LocalDateTime bT, List<Course> prerequisiteList) {
+
+		if ((name1 == null) || (faculty1 == null))
 			throw new NullPointerException();
-		if(name1.isEmpty())
+		if (name1.isEmpty())
 			throw new RuntimeException("The empty field was found!\n"); //$NON-NLS-1$
-		
+
 		this.name = name1;
 		this.id = id1;
 		this.faculty = faculty1;
 		this.points = acPoints;
 		this.aTerm = aT;
 		this.bTerm = bT;
-		
+
 		this.stuff = new ArrayList<>(st);
 		this.lessons = new ArrayList<>();
-		
+
 		this.listeners = new ArrayList<>();
 
 		this.projectHours = this.laboratoryHours = this.tutorialHours = this.lectureHours = 0;
-	}	
-	
+
+		this.prerequisites = new ArrayList<>(prerequisiteList);
+	}
+
 	protected void addLesson(Lesson ¢) {
 		this.lessons.add(¢);
 		addHours(¢);
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public String getId() {
 		return this.id;
 	}
-	
+
 	public String getFaculty() {
 		return this.faculty;
 	}
-	
+
 	public int getPoints() {
 		return this.points;
 	}
@@ -82,37 +86,36 @@ public class Course {
 		return this.bTerm;
 	}
 
-	
 	public List<StuffMember> getStuff() {
 		return new ArrayList<>(this.stuff);
 	}
-	
+
 	public List<Lesson> getLessons() {
 		return new ArrayList<>(this.lessons);
 	}
-	
+
 	public int getLectureHours() {
 		return this.lectureHours;
 	}
-	
+
 	public int getTutorialHours() {
 		return this.tutorialHours;
 	}
-	
+
 	public int getLaboratoryHours() {
 		return this.laboratoryHours;
 	}
-	
+
 	public int getProjectHours() {
 		return this.projectHours;
 	}
-	
+
 	public int getTotalHours() {
-		return this.laboratoryHours+this.lectureHours+this.projectHours+this.tutorialHours;
+		return this.laboratoryHours + this.lectureHours + this.projectHours + this.tutorialHours;
 	}
-	
+
 	protected void addHours(Lesson ¢) {
-		switch(¢.type) {
+		switch (¢.type) {
 		case LABORATORY:
 			this.laboratoryHours += ¢.duration;
 			break;
@@ -129,84 +132,87 @@ public class Course {
 			break;
 		}
 	}
-	
+
 	public static CourseBuilder giveCourseBuilderTo(@SuppressWarnings("unused") CourseLoader ¢) {
 		return new CourseBuilder();
 	}
-	
+
 	public void addListener(CourseListener ¢) {
 		this.listeners.add(¢);
 	}
-	
+
 	public void removeListener(CourseListener ¢) {
 		this.listeners.remove(¢);
 	}
-	
+
 	public void updateListeners() {
-		for(CourseListener ¢: this.listeners)
+		for (CourseListener ¢ : this.listeners)
 			¢.getUpdate(this);
 	}
-	
+
 	public static class CourseBuilder {
-		
+
 		protected String name = "";
 		protected String id = "";
 		protected String faculty = "";
 		protected int points;
 		protected LocalDateTime aTerm;
 		protected LocalDateTime bTerm;
-		
+
 		protected final List<StuffMember> stuff = new ArrayList<>();
 		protected final List<Lesson> lessons = new ArrayList<>();
+		
+		protected final List<Course> prerequisites  = new ArrayList<>();
 
 		public CourseBuilder setName(String ¢) {
 			this.name = ¢;
 			return this;
 		}
-		
+
 		public CourseBuilder setId(String ¢) {
-			this.id =¢;
+			this.id = ¢;
 			return this;
 		}
-		
+
 		public CourseBuilder setFaculty(String ¢) {
-			this.faculty =¢;
+			this.faculty = ¢;
 			return this;
 		}
-		
+
 		public CourseBuilder setPoints(int ¢) {
 			this.points = ¢;
 			return this;
 		}
-		
+
 		public CourseBuilder setATerm(LocalDateTime ¢) {
 			this.aTerm = ¢;
 			return this;
 		}
-		
+
 		public CourseBuilder setBTerm(LocalDateTime ¢) {
 			this.bTerm = ¢;
 			return this;
 		}
-		
+
 		public CourseBuilder addStuffMember(StuffMember ¢) {
-			if(!this.stuff.contains(¢))
+			if (!this.stuff.contains(¢))
 				this.stuff.add(¢);
 			return this;
 		}
+
 		public CourseBuilder addLesson(Lesson ¢) {
 			this.lessons.add(¢);
-			if(!this.stuff.contains(¢.representer))
+			if (!this.stuff.contains(¢.representer))
 				this.stuff.add(¢.representer);
 			return this;
 		}
-		
+
 		public Course build() {
-			Course $ = new Course(this.name, this.id, this.faculty, this.stuff, this.points, this.aTerm, this.bTerm);
-			for(Lesson ¢: this.lessons)
+			Course $ = new Course(this.name, this.id, this.faculty, this.stuff, this.points, this.aTerm, this.bTerm, this.prerequisites);
+			for (Lesson ¢ : this.lessons)
 				$.addLesson(¢);
 			return $;
 		}
 	}
-	
+
 }
