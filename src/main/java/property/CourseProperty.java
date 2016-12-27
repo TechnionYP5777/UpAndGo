170967fe391ad7e4b4cdeca0556311024e5097da
Nikita Dizhur <blenderon@gmail.com>
2016-12-27 17:06:10 +0200

    created CourseProperty that View listens for their changes in Model #61

---
+++ src/main/java/property/CourseProperty.java
@@ -0,0 +1,10 @@
+package property;
+
+public class CourseProperty {
+
+	public static String COURSE_LIST = "course_list";
+	public static String CHOSEN_LIST = "chosen_list";
+	public static String BAD_CMD = "bad_command";
+	public static String DETAILS = "details";
+
+}
