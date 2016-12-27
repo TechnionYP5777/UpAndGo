6efca1288fe0e61edd0752f5106b36cd917424b6
Nikita Dizhur <blenderon@gmail.com>
2016-12-27 18:05:35 +0200

    changed property name in CourseProperty #61

--- src/main/java/property/CourseProperty.java
+++ src/main/java/property/CourseProperty.java
@@ -4,7 +4,7 @@ public class CourseProperty {
 
 	public static String COURSE_LIST = "course_list";
 	public static String CHOSEN_LIST = "chosen_list";
-	public static String BAD_CMD = "bad_command";
+	public static String BAD_PROPERTY = "bad_property";
 	public static String DETAILS = "details";
 
 }
