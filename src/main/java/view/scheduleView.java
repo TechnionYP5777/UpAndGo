2c04c48c37b9fa1985e60d25ed503028f9b9f006
alexvitho <alexvitho@gmail.com>
2016-12-31 00:31:18 +0200

    add one lesson to table #65

--- src/main/java/view/scheduleView.java
+++ src/main/java/view/scheduleView.java
@@ -1,6 +1,10 @@
 package view;
 
 import java.awt.EventQueue;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -9,8 +13,10 @@ import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumn;
 import java.awt.Dimension;
-
 import java.awt.BorderLayout;
+import model.course.Lesson;
+import model.course.StuffMember;
+import model.course.WeekTime;
 
 public class scheduleView extends JFrame {
 
@@ -48,6 +54,13 @@ public class scheduleView extends JFrame {
 	 * Create the frame.
 	 */
 	public scheduleView() {
+		
+		ArrayList<Lesson> list= new ArrayList<>();
+//		list.add(new Lesson(new StuffMember("A", "a"),new WeekTime(DayOfWeek.MONDAY, LocalTime.of(8, 30)), new WeekTime( DayOfWeek.MONDAY ,LocalTime.of(10, 30)),"Taub 3", Lesson.Type.LECTURE,12,"MATAM"));
+//		list.add(new Lesson(new StuffMember("B", "b"),new WeekTime(DayOfWeek.WEDNESDAY, LocalTime.of(8, 30)), new WeekTime( DayOfWeek.WEDNESDAY ,LocalTime.of(10, 30)),"Taub 3", Lesson.Type.LECTURE,12,"MATAM"));
+//		list.add(new Lesson(new StuffMember("C", "c"),new WeekTime(DayOfWeek.MONDAY, LocalTime.of(12, 30)), new WeekTime( DayOfWeek.MONDAY ,LocalTime.of(14, 30)),"Taub 3", Lesson.Type.TUTORIAL,12,"MATAM"));
+		list.add(new Lesson(new StuffMember("D", "d"),new WeekTime(DayOfWeek.TUESDAY, LocalTime.of(10, 30)), new WeekTime( DayOfWeek.MONDAY ,LocalTime.of(11, 30)),"Taub 3", Lesson.Type.LECTURE,12,"MATAM"));
+
 		getContentPane().setMinimumSize(new Dimension(500, 300));
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		setMinimumSize(new Dimension(500, 300));
@@ -64,9 +77,29 @@ public class scheduleView extends JFrame {
 		for (int hour = 8, ¢ = 0; ¢ < rowNum; ++¢, ++hour)
 			table.getModel().setValueAt(getHour(hour), ¢, 6);
 
+		
+		insertCourses( list);
 		table.setCellSelectionEnabled(true);
 		getContentPane().add(table, BorderLayout.CENTER);
 
+	}
+
+	private void insertCourses(ArrayList<Lesson> list) {
+		if (list == null )
+			return;
+		for (Lesson l : list) {
+			int col = 6-l.getDay();
+			WeekTime s = l.getStartTime();
+			WeekTime e = l.getEndTime();
+			int row = s.getTime().getHour()-8;
+			String description = getDescriptionString(l);
+			table.getModel().setValueAt(description, row, col);
+		}
+		
+	}
+
+	private String getDescriptionString(Lesson l) {
+		return l.getCourse()+" "+l.getType()+"\n "+l.getRepresenter().getTitle()+" "+ l.getRepresenter().getFirstName()+" "+l.getRepresenter().getLastName()+"\n "+l.getPlace();
 	}
 
 	private static Object getHour(int hour) {
