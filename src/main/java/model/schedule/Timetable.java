4db59bfd2c6e0f8c5cc0f55245191576f82c00c3
danabramovich <sdan635z@campus.technion.ac.il>
2016-12-30 17:59:24 +0200

    implementation of Timetable #83

--- src/main/java/model/schedule/Timetable.java
+++ src/main/java/model/schedule/Timetable.java
@@ -1,5 +1,55 @@
 package model.schedule;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.course.LessonGroup;
+
+/**
+ * 
+ * this class holds a list of LessonGroup that do not clash between them
+ * hence they form a legel Schedule
+ *
+ */
 public class Timetable {
-	//TODO: implement
+	private List<LessonGroup> lessons;
+	
+	public Timetable(){
+		lessons=new ArrayList<>();
+	}
+/**
+ * this function keeps the invariant of Timetable, which is that they're no clashing lessons
+ * @param l - lesson to be added
+ * 
+ */
+	public void addLesson(LessonGroup ¢){
+		if(lessons.contains(¢)) // add equals to lessonsgroup
+			return;
+		lessons.add(¢);
+		if(!isNoClash())
+			lessons.remove(¢);
+	}
+	public void addLessons(List<LessonGroup> lessons){
+		this.lessons.addAll(lessons);
+		if(!isNoClash())
+			this.lessons.removeAll(lessons);
+	}
+	
+	public void removeLesson(LessonGroup ¢) {
+		lessons.remove(¢);
+	}
+	
+	public List<LessonGroup> getLessons(){
+		return lessons;
+	}
+	
+	private boolean isNoClash(){
+		for(int i=0; i < lessons.size(); ++i){
+			for(int j=i+1; j < lessons.size(); ++j)
+				if (!lessons.get(i).isCLashWIth(lessons.get(j)))
+					return false;
+		}
+		return true;
+	}
+	
 }
