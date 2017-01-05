0457034f740788df16bb3d8d66ddbf67e8b520f7
Koby Ben Shimol <kobybs@gmail.com>
2017-01-05 12:41:53 +0200

    close #88 optimization done and tested

--- src/test/java/model/logic/SchedulerTest.java
+++ src/test/java/model/logic/SchedulerTest.java
@@ -88,5 +88,23 @@ CourseLoader cr;
 		
 		assertNull(s);
 	}
+	
+	@Test
+	public void test_e() {
+		cr = new XmlCourseLoader("resources/testXML/schedulerTest7.XML");
+		
+		List<Course> courses = new ArrayList<>(cr.loadAllCourses().values());
+		System.out.println(courses);
+		
+		Schedule s = Scheduler.schedule(courses, new ArrayList<TimeConstraint>());
+		System.out.println(s);
+		
+		assert s.getLessonGroups().get(0).getLessons().get(0).getStartTime().equals(new WeekTime(DayOfWeek.SUNDAY, LocalTime.of(10, 00)));
+		assert s.getLessonGroups().get(1).getLessons().get(0).getStartTime().equals(new WeekTime(DayOfWeek.WEDNESDAY, LocalTime.of(13, 00)));
+		assert s.getLessonGroups().get(2).getLessons().get(0).getStartTime().equals(new WeekTime(DayOfWeek.TUESDAY, LocalTime.of(11, 00)));
+		assert s.getLessonGroups().get(3).getLessons().get(0).getStartTime().equals(new WeekTime(DayOfWeek.SUNDAY, LocalTime.of(14, 00)));
+		
+		//assertNull(s);
+	}
 
 }
