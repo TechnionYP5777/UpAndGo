76cffbcaf179977ce2479f73d16a021ae037188d
leyaniv <leyaniv@gmail.com>
2017-01-07 17:46:18 +0200

    parsing faculty names and ids from UG #92

---
+++ src/test/java/parse/UgParserTest.java
@@ -0,0 +1,16 @@
+package parse;
+
+import org.junit.Test;
+
+import model.Faculty;
+
+public class UgParserTest {
+
+	@Test
+	@SuppressWarnings("static-method")
+	public void test() {
+		for(Faculty ¢ : UgParser.getFaculties())
+			System.out.println(¢.getId() + " " + ¢.getName());
+	}
+
+}
