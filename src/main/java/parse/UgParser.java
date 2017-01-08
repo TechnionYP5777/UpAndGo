1e79016a27ac9a0b358daab31afd08ccc516176e
leyaniv <leyaniv@gmail.com>
2017-01-08 05:56:29 +0200

    printing courses name and ids of current semester as scraped from UG #93

--- src/main/java/parse/UgParser.java
+++ src/main/java/parse/UgParser.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import model.Faculty;
@@ -30,6 +34,54 @@ public class UgParser {
 		}
 		
 		return $;
+		
+	}
+	
+	public static void getCoursesNamesAndID(){
+		List<Faculty> faculties = getFaculties();
+		Document courses = null;
+
+		Map<String,String> dataMap = new HashMap();
+		dataMap.put("CNM", "");
+		dataMap.put("CNO", "");
+		dataMap.put("PNT", "");
+		dataMap.put("FAC", "");
+		dataMap.put("LLN", "");
+		dataMap.put("LFN", "");
+		dataMap.put("SEM", "201602");
+		dataMap.put("RECALL", "Y");
+		dataMap.put("D1", "on");
+		dataMap.put("D2", "on");
+		dataMap.put("D3", "on");
+		dataMap.put("D4", "on");
+		dataMap.put("D5", "on");
+		dataMap.put("D6", "on");
+		dataMap.put("FTM", "");
+		dataMap.put("TTM", "");
+		dataMap.put("SIL", "");
+		dataMap.put("OPTCAT", "on");
+		dataMap.put("OPTSEM", "on");
+		dataMap.put("OPTSTUD", "on");
+		dataMap.put("doSearch", "Y");
+		
+		for (Faculty faculty : getFaculties()){
+			try {
+				dataMap.put("FAC", faculty.getId());
+				courses = Scraper.getSearchResults(UG_SEARCH_URL,dataMap);			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			for (Element course : courses.getElementsByClass("result-row")){
+				System.out.println(course.childNode(1).childNode(1).childNode(0));
+				System.out.println(course.childNode(3).childNode(0).toString().replace("\n","").substring(1));
+
+
+			}
+		}
+		
+
+		
 		
 	}
 }
