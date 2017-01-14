94ebcb76dc8315c7e988decafe758e4937738101
SapirBismot <Sapirbismot@gmail.com>
2017-01-14 22:43:32 +0200

    continue parsing #106

--- src/main/java/model/course/Course.java
+++ src/main/java/model/course/Course.java
@@ -37,7 +37,8 @@ public class Course {
 
 	protected final List<Course> prerequisites;
 	protected final List<Course> corequisites;
-
+	protected boolean done;
+	
 	// TODO: create interface LessonGroup as mediator between Lessons and Course
 
 	public Course(String name1, String id1, String faculty1, List<StuffMember> st, double acPoints, LocalDateTime aT,
@@ -156,6 +157,13 @@ public class Course {
 	public void updateListeners() {
 		for (CourseListener ¢ : this.listeners)
 			¢.getUpdate(this);
+	}
+	
+	public boolean getDone() {
+		return done;
+	}
+	public void MarkDone() {
+		done = true;
 	}
 	
 	@Override
--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,7 +1,9 @@
 package user;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import model.course.Course;
 
@@ -12,13 +14,13 @@ import org.jsoup.parser.Parser;
 import catalog.Catalog;
 
 public class User {
-	List<Course> courses;
+	Set<String> courses;
 	Catalog catalog;
 	String name;
 	
 	public User(String grades, Catalog c) {
 		catalog = c;
-		courses = new ArrayList<>();
+		courses = new HashSet<>();
 		int count = 0;
 		Boolean flag = false;
 		for (String ¢ : grades.split("\\r?\\n")) {
@@ -32,16 +34,17 @@ public class User {
 					continue;
 				}
 				String[] grade = ¢.split("\\t");
-				if (grade[0].contains("םילשה אל") || Integer.parseInt(grade[0]) < 55) {
+				if (grade[0].contains("םילשה אל") || Integer.parseInt(grade[0]) < 55) { //* TODO: check about not completed courses *//
+					if (!grade[0].contains("םילשה אל") && courses.contains(grade[2]))
+						courses.remove(grade[2]);
 					continue;
-				}	
+				}
+				courses.add(grade[2]);
 			}
-			if (¢.contains("ןויצ")) {
+			if (¢.contains("ןויצ"))
 				flag = true;
-			}
 		}
-		
-
+		System.out.print(courses);
 	}
 	
 	public String getName() {
