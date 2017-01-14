5a2ff02e8d44ae4aecb4b9e9565fa6875bc63f00
Nikita Dizhur <blenderon@gmail.com>
2017-01-14 16:49:04 +0200

    added load methods for TimeTableModel and wired them with TTController #103

--- src/main/java/model/TimeTableModel.java
+++ src/main/java/model/TimeTableModel.java
@@ -4,30 +4,55 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.HashMultimap;
+
+import logic.Scheduler;
 import model.course.Course;
 
 public class TimeTableModel implements Model {
 
-	List<Course> courses = new ArrayList<>();
+	List<Course> courses;
+
+	boolean isDaysoffCount;
+	boolean isBlankSpaceCount;
+	
+	protected HashMultimap<String, PropertyChangeListener> listenersMap;
 	
 	public TimeTableModel() {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void addPropertyChangeListener(@SuppressWarnings("unused") String property, @SuppressWarnings("unused") PropertyChangeListener __) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void removePropertyChangeListener(@SuppressWarnings("unused") String property, @SuppressWarnings("unused") PropertyChangeListener __) {
-		// TODO Auto-generated method stub
-
+		this.courses = new ArrayList<>();
+		this.listenersMap = HashMultimap.create();
+		this.isBlankSpaceCount = this.isDaysoffCount = false;
 	}
 	
 	public void setCourses(List<Course> ¢) {
 		courses = new ArrayList<>(¢);
 	}
+	
+	public void loadSchedule() {
+//		Scheduler.sortedBy(Scheduler.getTimetablesList(courses), isDaysoffCount, isBlankSpaceCount);
+		// TODO implement it
+	}
+	
+	public void loadNextSchedule() {
+		// TODO implement it
+	}
 
+	public void loadPrevSchedule() {
+		// TODO implement it
+	}
+	
+	@Override
+	public void addPropertyChangeListener(String property, PropertyChangeListener l) {
+		if (property == null || l == null)
+			throw new NullPointerException();
+		this.listenersMap.put(property, l);
+
+	}
+
+	@Override
+	public void removePropertyChangeListener(String property, PropertyChangeListener l) {
+		if (property != null && l != null && this.listenersMap.containsKey(property))
+			this.listenersMap.remove(property, l);
+
+	}
 }
