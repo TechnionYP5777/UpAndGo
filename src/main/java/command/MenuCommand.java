b702692d38fe86c202ae84bcef6330ce837dcda3
danabramovich <sdan635z@campus.technion.ac.il>
2017-04-02 21:08:26 +0300

    retreived command and property #173

---
+++ src/main/java/command/MenuCommand.java
@@ -0,0 +1,17 @@
+package command;
+
+/**
+ * 
+ * @author Lida Piatigorski
+ * @since 14-01-2017
+ * 
+ * Class that represents all types of commands that {@link #MenuView} can send to {@link #MenuController}.
+ * 
+ */
+public class MenuCommand {
+	public static final String LOAD_CATALOG = "load_catalog";
+	public static final String LOAD_GILAYON = "load_gilayon";
+	public static final String SAVE = "save";
+	public static final String BAD_CMD = "bad_command";
+
+}
