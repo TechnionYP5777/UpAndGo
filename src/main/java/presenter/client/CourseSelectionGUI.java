512a0207725fdef97236c5ac2f41f0b3319ffcca
danabramovich <sdan635z@campus.technion.ac.il>
2017-04-07 19:19:31 +0300

    basic course selecyion UI #214

--- src/main/java/presenter/client/CourseSelectionGUI.java
+++ src/main/java/presenter/client/CourseSelectionGUI.java
@@ -1,5 +1,9 @@
 package presenter.client;
 
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+
 /**
  * 
  * @author danabra
@@ -13,17 +17,35 @@ package presenter.client;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class CourseSelectionGUI extends VerticalPanel {
-    private ListBox lb = new ListBox();
+public class CourseSelectionGUI extends LayoutPanel {
+    private ListBox ccl = new ListBox();
+    private Label cc = new Label("קורסים שנבחרו:");
+    private ListBox scl = new ListBox();
+    private Label sc = new Label("בחר קורסים:");
     public CourseSelectionGUI(){
     	InitializePanel();
     }
     private void InitializePanel(){
-	    lb.addItem("foo");
-	    lb.addItem("bar");
-	    lb.addItem("baz");
-	    lb.addItem("toto");
-	    lb.addItem("tintin");
-	    this.add(lb);
+    	ccl.setMultipleSelect(true);
+    	ccl.addItem("קורס1");
+    	ccl.addItem("קורס2");
+    	ccl.addItem("קורס3");
+    	ccl.addItem("קורס4");
+    	ccl.setWidth("100%");
+    	ccl.setHeight("25em");
+    	scl.setMultipleSelect(true);
+    	scl.addItem("קורס1");
+    	scl.addItem("קורס2");
+    	scl.addItem("קורס3");
+    	scl.addItem("קורס4");
+    	scl.setWidth("100%");
+    	this.add(cc);
+	    this.add(ccl);
+	    this.add(sc);
+	    this.add(scl);
+	    this.setWidgetTopBottom(cc, 0, Unit.EM, 0, Unit.EM);
+	    this.setWidgetTopBottom(ccl, 1.5,  Unit.EM, 0, Unit.EM);
+	    this.setWidgetTopBottom(sc, 30,  Unit.EM, 0, Unit.EM);
+	    this.setWidgetTopBottom(scl, 31.5,  Unit.EM, 0, Unit.EM);
     }
 }
--- src/main/java/presenter/client/Webapp.java
+++ src/main/java/presenter/client/Webapp.java
@@ -1,17 +1,20 @@
 package presenter.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class Webapp implements EntryPoint {
-	private HorizontalPanel p = new HorizontalPanel();
+	private LayoutPanel p = new LayoutPanel();
 	@Override
 	public void onModuleLoad() {
-		p.add(new CourseSelectionGUI());
-		RootPanel.get().add(p);
+		CourseSelectionGUI cc = new CourseSelectionGUI();
+		p.add(cc);
+		p.setWidgetRightWidth(cc, 1, Unit.EM, 20, Unit.PCT);
+		p.setWidgetTopHeight(cc, 1, Unit.EM, 100, Unit.PCT);
+		RootLayoutPanel.get().add(p);
 	}
 
 }
