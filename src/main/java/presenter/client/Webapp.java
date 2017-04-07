70975c09c2e707bf3d47e9cb87dccc03bfc6de43
danabramovich <sdan635z@campus.technion.ac.il>
2017-04-07 13:04:55 +0300

    expiriencing with gui #214

--- src/main/java/presenter/client/Webapp.java
+++ src/main/java/presenter/client/Webapp.java
@@ -1,14 +1,19 @@
 package presenter.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Webapp implements EntryPoint {
-
+	private Label l = new Label("Hello World!!!");
+	private Label l2 = new Label("Hello World!!!");
+	private HorizontalPanel p = new HorizontalPanel();
 	@Override
 	public void onModuleLoad() {
-		RootPanel.get().add(new Label("Hello World"));
+		p.add(l);
+		p.add(l2);
+		RootPanel.get().add(p);
 	}
 
 }
