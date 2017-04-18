2923edb200af81ff55e41ce713159b17ec842c9b
OmriBS <omri.benshmuel@gmail.com>
2017-04-18 14:01:36 +0300

    creating presenter to the scedulerView

---
+++ src/main/java/upandgo/client/presenter/SchedulerPresenter.java
@@ -0,0 +1,25 @@
+package upandgo.client.presenter;
+
+import com.google.gwt.user.client.ui.Panel;
+
+public class SchedulerPresenter implements Presenter {
+
+	@Override
+	public void bind() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unbind() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void go(Panel panel) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
