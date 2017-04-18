4dfcb8be4771bb669daaa9ddab41a13229071f72
OmriBS <omri.benshmuel@gmail.com>
2017-04-18 15:06:36 +0300

    implementing go method in schedulerPresenter #212

--- src/main/java/upandgo/client/presenter/SchedulerPresenter.java
+++ src/main/java/upandgo/client/presenter/SchedulerPresenter.java
@@ -1,9 +1,32 @@
 package upandgo.client.presenter;
 
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+
+import upandgo.client.view.CourseListView;
+
+/**
+ * 
+ * @author Omri Ben Shmuel
+ * @since 18-04-17
+ * 
+ * A concrete presenter for {@link schedulerView}.
+ * 
+ */
 
 public class SchedulerPresenter implements Presenter {
-
+	
+	private final Display view;
+	
+	public interface Display {
+		public Widget asWidget();
+	}
+	
+	public SchedulerPresenter(Display view) {
+		this.view = view;
+	}
+	
+	
 	@Override
 	public void bind() {
 		// TODO Auto-generated method stub
@@ -18,7 +41,7 @@ public class SchedulerPresenter implements Presenter {
 
 	@Override
 	public void go(Panel panel) {
-		// TODO Auto-generated method stub
+		panel.add(view.asWidget());
 
 	}
 
