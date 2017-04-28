fafbd16b075215c8218c413091a6cceb6b3f4773
Koby Ben Shimol <kobybs@gmail.com>
2017-04-28 15:19:54 +0300

    #215 try to fix logic problem

--- src/main/java/upandgo/client/TimeTableGUI.java
+++ src/main/java/upandgo/client/TimeTableGUI.java
@@ -248,7 +248,7 @@ public class TimeTableGUI extends LayoutPanel {
  			t.getFlexCellFormatter().setRowSpan(1, day, 22);
  			t.getCellFormatter().addStyleName(1, day, ttStyle.hasEvent());
  			
- 			int differenceInMinutes = WeekTime.difference(LocalTime.of(10, 30), LocalTime.of(8, 30));
+ 			//int differenceInMinutes = WeekTime.difference(LocalTime.of(10, 30), LocalTime.of(8, 30));
  	 		
  			return;
  		}
