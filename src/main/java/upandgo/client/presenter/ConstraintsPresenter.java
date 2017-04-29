05c0a5ff63f51cf71afbd14258c9d949d78131f1
OmriBS <omri.benshmuel@gmail.com>
2017-04-29 11:21:55 +0300

    creating constraints presenter #212

--- src/main/java/upandgo/client/presenter/ConstraintsPresenter.java
+++ src/main/java/upandgo/client/presenter/ConstraintsPresenter.java
@@ -1,9 +1,35 @@
 package upandgo.client.presenter;
 
+import com.google.gwt.event.dom.client.HasDoubleClickHandlers;
+import com.google.gwt.event.dom.client.HasMouseMoveHandlers;
+import com.google.gwt.event.dom.client.HasMouseOutHandlers;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.web.bindery.event.shared.EventBus;
+
+import upandgo.client.CoursesServiceAsync;
+import upandgo.client.presenter.SchedulerPresenter.Display;
 
 public class ConstraintsPresenter implements Presenter {
-
+	
+	private final Display view;
+	private final EventBus eventBus;
+		
+	public interface Display {
+		public boolean isDayOffChecked();
+		public boolean isMinWindowsChecked();
+		public boolean isStartTimeChecked();
+		public boolean isFinishTimeChecked();
+		public String getReqStartTime(); // result in format HH:MM
+		public String getReqFinishTime(); // result in format HH:MM
+		public Widget asWidget();
+	}
+	
+	public ConstraintsPresenter(Display view, EventBus eventBus) {
+		this.eventBus = eventBus; 
+		this.view = view;
+	}
+	
 	@Override
 	public void bind() {
 		// TODO Auto-generated method stub
