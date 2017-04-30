e8a9abbe52a273084ccdca94af7375eed24f96b8
Koby Ben Shimol <kobybs@gmail.com>
2017-04-30 12:02:12 +0300

    #235 write LocalTime

---
+++ src/main/java/upandgo/shared/entities/LocalTime.java
@@ -0,0 +1,42 @@
+package upandgo.shared.entities;
+
+public class LocalTime implements Comparable<LocalTime> {
+	private int hour;
+	private int minute;
+	
+	private LocalTime(int hour, int minute){
+		this.hour = hour;
+		this.minute = minute;
+	}
+	
+	/**
+	 * Obtains an instance of LocalTime from an hour and minute.
+		This returns a LocalTime with the specified hour and minute. The second and nanosecond fields will be set to zero.
+		Parameters:
+		hour - the hour-of-day to represent, from 0 to 23
+		minute - the minute-of-hour to represent, from 0 to 59
+		Returns:
+		the local time, not null
+		Throws:
+		DateTimeException - if the value of any field is out of range
+	 */
+	public static LocalTime of(int hour, int minute){
+		return new LocalTime(hour, minute);
+	}
+
+	@Override
+	public int compareTo(LocalTime o) {
+		if(this.hour != o.hour){
+			return this.hour - o.hour;
+		}
+		return this.minute - o.minute;
+	}
+	
+	public int getHour(){
+		return hour;
+	}
+	
+	public int getMinute(){
+		return minute;
+	}
+}
