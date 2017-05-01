f2b07b70405b40ad228ec95da09629dc6564213d
Koby Ben Shimol <kobybs@gmail.com>
2017-05-01 13:31:36 +0300

    #215 more refactoring

--- src/main/java/upandgo/client/TimeTableGUI.java
+++ src/main/java/upandgo/client/TimeTableGUI.java
@@ -141,53 +141,7 @@ public class TimeTableGUI extends LayoutPanel {
 		t.setText(9, 3, "");
 		t.getFlexCellFormatter().setRowSpan(9, 3, 14);
 		t.getCellFormatter().addStyleName(9, 3, ttStyle.noEvent());
-		/*t.setText(9, 2, "");
-		t.getFlexCellFormatter().setRowSpan(9, 2, 2);
-		t.getCellFormatter().addStyleName(9, 2, "emptyCell");
-		
-		t.setText(11, 2, "מבוא לכלכה, תרגול, כיתה 301 ועוד דברים בדיקת חריגה מגבולות בדיקת חריגה מגבולות בדיקת חריגה");
-		t.getFlexCellFormatter().setRowSpan(11, 2, 2);
-		t.getCellFormatter().addStyleName(11, 2, "notEmptyCell1");
-		
-		t.setText(13, 2, "");
-		t.getFlexCellFormatter().setRowSpan(13, 2, 10);
-		t.getCellFormatter().addStyleName(13, 2, "emptyCell");*/
-		
-		
-		
-    	/*for(int day = 4; day < 7; day++){
-	    	for(int i = 1; i<12; i++){
-	    		t.setText(2*i-1, day, "");
-	    		t.getFlexCellFormatter().setRowSpan(2*i-1, day, 2);
-	    		String cssSign = (i % 2 == 0) ? "insideEmptyCellEven":"insideEmptyCellOdd";
-	    		t.getCellFormatter().addStyleName(2*i-1, day, cssSign);
-	    		
-	    	}
-    	}*/
-    	
-    	
-    	
-    	
-    	
-    	/*t.setText(0, 0, "upper-left corner");
-	    t.setText(0, 1, "center corner");
-	    t.setText(0, 2, "right corner");
-	    //t.setText(0, 1, "upper-center");
-	    t.setText(1, 1, "center-left");
-	    t.setText(2, 2-1, "bottom-right corner");*/
 
-	    // Let's put a button in the middle...
-	    //t.setWidget(1, 0, new Button("Wide Button"));
-
-	    // ...and set it's column span so that it takes up the whole row.
-	    //t.getFlexCellFormatter().setRowSpan(1, 0, 2);
-	    /*t.getRowFormatter().addStyleName(0, "headerRowStyle");
-	    t.getRowFormatter().addStyleName(1, "headerRowStyle");
-	    t.getRowFormatter().addStyleName(2, "headerRowStyle");*/
-	    /*t.getCellFormatter().addStyleName(0, 0, "watchListNumericColumn");
-	    t.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
-	    t.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");*/
-	    //t.addStyleName("table-bordered");
 	    t.addStyleName(ttStyle.timeTable());
 	    
 	    
@@ -203,10 +157,6 @@ public class TimeTableGUI extends LayoutPanel {
 	    drawDay(lessons, 4);
 
 	    this.add(t);
-	    /*this.setWidgetTopBottom(cc, 0, Unit.EM, 0, Unit.EM);
-	    this.setWidgetTopBottom(ccl, 1.5,  Unit.EM, 0, Unit.EM);
-	    this.setWidgetTopBottom(sc, 30,  Unit.EM, 0, Unit.EM);
-	    this.setWidgetTopBottom(scl, 31.5,  Unit.EM, 0, Unit.EM);*/
     }
     
     
