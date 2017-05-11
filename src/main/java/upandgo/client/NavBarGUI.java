5d92e7d4ced27bb50a3975d15ba95a8a8ec6f73b
Koby Ben Shimol <kobybs@gmail.com>
2017-05-11 15:33:14 +0300

    #242 first diaalog

--- src/main/java/upandgo/client/NavBarGUI.java
+++ src/main/java/upandgo/client/NavBarGUI.java
@@ -7,12 +7,62 @@ import org.gwtbootstrap3.client.ui.NavbarCollapse;
 import org.gwtbootstrap3.client.ui.NavbarHeader;
 import org.gwtbootstrap3.client.ui.NavbarLink;
 import org.gwtbootstrap3.client.ui.NavbarText;
+import org.gwtbootstrap3.client.ui.SuggestBox;
 import org.gwtbootstrap3.client.ui.constants.Pull;
 import org.gwtbootstrap3.client.ui.gwt.FlowPanel;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import upandgo.client.Resources.NavBarStyle;
 
 public class NavBarGUI extends FlowPanel{
+	
+	
+	 private static class MyDialog extends DialogBox {
+
+	      public MyDialog() {
+	         // Set the dialog box's caption.
+	         setText("My First Dialog");
+
+	         // Enable animation.
+	         setAnimationEnabled(true);
+
+	         // Enable glass background.
+	         setGlassEnabled(true);
+
+	         // DialogBox is a SimplePanel, so you have to set its widget 
+	         // property to whatever you want its contents to be.
+	         Button ok = new Button("OK");
+	         ok.addClickHandler(new ClickHandler() {
+	            @Override
+				public void onClick(ClickEvent event) {
+	               MyDialog.this.hide();
+	            }
+	         });
+
+	         Label label = new Label("Please enter username and password");
+
+	         VerticalPanel panel = new VerticalPanel();
+	         panel.setHeight("20em");
+	         panel.setWidth("300");
+	         panel.setSpacing(10);
+	         panel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+	         panel.add(label);
+	         panel.add(ok);
+
+	         setWidget(panel);
+	      }
+	   }
+	
 	
 	private NavBarStyle nvStyle = Resources.INSTANCE.navBarStyle();
 	
@@ -46,6 +96,22 @@ public class NavBarGUI extends FlowPanel{
 		navbar.add(navbarCol);
 		this.add(navbar);
 		this.addStyleName(nvStyle.NavBarPanel());
+		
+		
+		signIn.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent arg0) {
+				// TODO Auto-generated method stub
+				MyDialog myDialog = new MyDialog();
+
+	            int left = Window.getClientWidth()/ 2;
+	            int top = Window.getClientHeight()/ 2;
+	            myDialog.setPopupPosition(left, top);
+	            myDialog.show();
+			}
+		});
+		
 	}
 
 }
