91da1a5d0c9eece46fddc209d9361ba72fd47fae
leyaniv <leyaniv@gmail.com>
2017-05-12 11:11:23 +0300

    added gwt-bootstrap-extras. modal for login and new login button #243

--- src/main/java/upandgo/client/SchedualerControlsView.java
+++ src/main/java/upandgo/client/SchedualerControlsView.java
@@ -4,7 +4,6 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
 import upandgo.client.Resources.SchedualerControlsStyle;
