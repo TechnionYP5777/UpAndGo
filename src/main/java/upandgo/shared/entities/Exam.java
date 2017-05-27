adebd577bbb1db9030e8c660723586b2c7b1b5eb
OmriBS <omri.benshmuel@gmail.com>
2017-05-27 21:49:31 +0300

    Presenter: add method to Exam in order to display dates Issue #260

--- src/main/java/upandgo/shared/entities/Exam.java
+++ src/main/java/upandgo/shared/entities/Exam.java
@@ -2,6 +2,8 @@ package upandgo.shared.entities;
 
 
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 
@@ -11,6 +13,8 @@ public class Exam implements IsSerializable{
 	private String dayOfMonth;
 	private String year;
 	
+	private int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
 	public Exam() {
 		month = Month.JANUARY;
 		dayOfMonth = year = "";
@@ -33,6 +37,36 @@ public class Exam implements IsSerializable{
 	
 	public String getYear() {
 		return year;
+	}
+	
+	private String compareString() {
+		return year + month.toString() + dayOfMonth;
+	}
+	
+	private static Boolean isLeap(String year) {
+		int yearnum = Integer.parseInt(year);
+		if (((yearnum % 4 == 0) && (yearnum % 100 != 0)) || (yearnum % 400 == 0)) {
+			return true;
+		}
+		return false;
+	}
+	public int compare(Exam exam) {
+		return this.compareString().compareTo(exam.compareString());
+	}
+	
+	//the exams should be in order, exama must be early then examb
+	// both exams must be in the same year - for now
+	public int daysBetweenExams (Exam exama, Exam examb) {
+		int res = 0;
+		if (isLeap(exama.getYear())) {
+			monthDays[1] = 29;
+		}
+		for (int i = month.toInt(exama.getMonth()); i < (month.toInt(examb.getMonth()) -1 ); i++) {
+			res += monthDays[i];
+		}
+		res += (monthDays[month.toInt(exama.getMonth()) -1 ] - Integer.parseInt(exama.getDay())) + Integer.parseInt(examb.getDay());
+		monthDays[1] = 28;
+		return res;
 	}
 	
 }
--- src/main/java/upandgo/shared/entities/Month.java
+++ src/main/java/upandgo/shared/entities/Month.java
@@ -7,4 +7,62 @@ public enum Month implements IsSerializable{
 	JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
 	
 	Month(){}
+	
+	public static int toInt(Month month) {
+		switch (month) {
+		case JANUARY:
+			return 1;
+		case FEBRUARY:
+			return 2;
+		case MARCH:
+			return 3;
+		case APRIL:
+			return 4;
+		case MAY:
+			return 5;
+		case JUNE:
+			return 6;
+		case JULY:
+			return 7; 
+		case AUGUST:
+			return 8;
+		case SEPTEMBER:
+			return 9;
+		case OCTOBER:
+			return 10;
+		case NOVEMBER:
+			return 11;
+		default:
+			return 12;
+		}
+	}
+	
+	public static String toString(Month month) {
+		switch (month) {
+		case JANUARY:
+			return "01";
+		case FEBRUARY:
+			return "02";
+		case MARCH:
+			return "03";
+		case APRIL:
+			return "04";
+		case MAY:
+			return "05";
+		case JUNE:
+			return "06";
+		case JULY:
+			return "07"; 
+		case AUGUST:
+			return "08";
+		case SEPTEMBER:
+			return "09";
+		case OCTOBER:
+			return "10";
+		case NOVEMBER:
+			return "11";
+		default:
+			return "12";
+		}
+	}
 }
