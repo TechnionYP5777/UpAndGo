f8f15fd2bd6d1eb46195ff20f03fb7163bda8128
Nikita Dizhur <blenderon@gmail.com>
2017-05-28 14:40:39 +0300

    server, presenter: add Gmail user authentication
    add Gmail user authentication.
    Issue: #262

--- src/main/java/upandgo/server/LoginServiceImpl.java
+++ src/main/java/upandgo/server/LoginServiceImpl.java
@@ -1,5 +1,6 @@
 package upandgo.server;
 
+
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -8,30 +9,28 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import upandgo.client.LoginInfo;
 import upandgo.client.LoginService;
 
-public class LoginServiceImpl extends RemoteServiceServlet implements
-    LoginService {
+public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
 
-  /**
+	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 4093467154679515643L;
+	private static final long serialVersionUID = -7298480955963060988L;
 
-@Override
-public LoginInfo login(String requestUri) {
-    UserService userService = UserServiceFactory.getUserService();
-    User user = userService.getCurrentUser();
-    LoginInfo loginInfo = new LoginInfo();
+	@Override
+	public LoginInfo login(String requestUri) {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		LoginInfo loginInfo = new LoginInfo();
 
-    if (user != null) {
-      loginInfo.setLoggedIn(true);
-      loginInfo.setEmailAddress(user.getEmail());
-      loginInfo.setNickname(user.getNickname());
-      loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
-    } else {
-      loginInfo.setLoggedIn(false);
-      loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
-    }
-    return loginInfo;
-  }
-
+		if (user != null) {
+			loginInfo.setLoggedIn(true);
+			loginInfo.setEmailAddress(user.getEmail());
+			loginInfo.setNickname(user.getNickname());
+			loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+		} else {
+			loginInfo.setLoggedIn(false);
+			loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+		}
+		return loginInfo;
+	}
 }
