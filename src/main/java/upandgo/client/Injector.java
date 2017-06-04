a021fc2c81d3600054310d903df3549661c934e4
Nikita Dizhur <blenderon@gmail.com>
2017-06-04 09:54:51 +0300

    presenter, server: add Gmail user authentication
    added navbarpresenter and buttons for log-in and out
    
    Issue: #262

--- src/main/java/upandgo/client/Injector.java
+++ src/main/java/upandgo/client/Injector.java
@@ -6,6 +6,7 @@ import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
 
 import upandgo.client.presenter.CourseListPresenter;
+import upandgo.client.presenter.NavBarPresenter;
 import upandgo.client.presenter.SchedulerPresenter;
 
 @GinModules(InjectorModule.class)
@@ -16,6 +17,7 @@ public interface Injector extends Ginjector {
 	
 	
 	public EventBus getEventBus();
+	public NavBarPresenter.Display getNavBarPresenterDisplay();
 	public CourseListPresenter.Display getCourseListPresenterDisplay();
 	public SchedulerPresenter.Display getSchedulerPresenterDisplay();
 	public CourseListPresenter getCourseListPresenter();
