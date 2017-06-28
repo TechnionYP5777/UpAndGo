33adb46b35fda1a5523a1888abdb62c6da8a801a
OmriBS <omri.benshmuel@gmail.com>
2017-06-28 20:14:04 +0300

    Testing: adding back some deleted classes Issue: #317

---
+++ src/main/java/upandgo/server/LoginServiceImpl.java
@@ -0,0 +1,36 @@
+//package upandgo.server;		
+//		
+//		
+//import com.google.appengine.api.users.User;		
+//import com.google.appengine.api.users.UserService;		
+//import com.google.appengine.api.users.UserServiceFactory;		
+//import com.google.gwt.user.server.rpc.RemoteServiceServlet;		
+//		
+//import upandgo.client.LoginInfo;		
+//import upandgo.client.LoginService;		
+//		
+//public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {		
+//		
+//	/**		
+//	 * 		
+//	 */		
+//	private static final long serialVersionUID = -7298480955963060988L;		
+//		
+//	@Override		
+//	public LoginInfo login(String requestUri) {		
+//		UserService userService = UserServiceFactory.getUserService();		
+//		User user = userService.getCurrentUser();		
+//		LoginInfo loginInfo = new LoginInfo();		
+//		
+//		if (user != null) {		
+//			loginInfo.setLoggedIn(true);		
+//			loginInfo.setEmailAddress(user.getEmail());		
+//			loginInfo.setNickname(user.getNickname());		
+//			loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));		
+//		} else {		
+//			loginInfo.setLoggedIn(false);		
+//			loginInfo.setLoginUrl(userService.createLoginURL(requestUri));		
+//		}		
+//		return loginInfo;		
+//	}		
+//}
