0b66f5a0268e9d49c642da48d46587e7e7e3807b
OmriBS <omri.benshmuel@gmail.com>
2017-06-30 13:18:11 +0300

    .\bin

---
+++ src/test/java/upandgo/server/model/course/CourseServiceTest.java
@@ -0,0 +1,21 @@
+package upandgo.server.model.course;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import upandgo.server.CoursesServiceImpl;
+import upandgo.shared.entities.Exam;
+import upandgo.shared.entities.Semester;
+import upandgo.shared.entities.course.CourseId;
+
+public class CourseServiceTest {
+
+	@Test
+	public void test() {
+		CoursesServiceImpl course = new CoursesServiceImpl();
+		course.getCourseDetails(Semester.WINTER17, new CourseId("234107", "אנליזה נומרית 1", new Exam("13:00 08 08 2017 4"), new Exam("13:00 08 10 2017 4")));
+		assertEquals("emptyWe have got our credentials!",course.getSomeString());
+	}
+
+}
